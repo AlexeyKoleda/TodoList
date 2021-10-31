e4ff7d9ad0d7fde01f5376dea446b62d3965fb68
Alexey Koleda <alexeykoleda@icloud.com>
2021-10-31 20:43:09 +0300

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Alexey Koleda on 31.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
